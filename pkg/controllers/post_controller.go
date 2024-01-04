09e5a324d6d4f7d5e7534bf8aac8c3210496defc
soulin maikadua <104555691+soulincsl@users.noreply.github.com>
2024-01-04 14:55:01 +0700

    office commit

--- pkg/controllers/post_controller.go
+++ pkg/controllers/post_controller.go
@@ -1,12 +1,19 @@
 package controllers
 
 import (
+	"fmt"
+	"time"
+
+	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
 	"github.com/google/uuid"
 	"github.com/soulinmaikadua/my-go-fiber/pkg/configs"
+	"github.com/soulinmaikadua/my-go-fiber/pkg/models"
+	"github.com/soulinmaikadua/my-go-fiber/pkg/utils"
 )
 
 func GetPosts(c *fiber.Ctx) error {
+	claims := utils.GetToken(c)
 	// create connection
 	db, err := configs.OpenDBConnection()
 	if err != nil {
@@ -17,10 +24,12 @@ func GetPosts(c *fiber.Ctx) error {
 			"data":    nil,
 		})
 	}
+	parsedUUID, _ := uuid.Parse(claims["id"].(string))
 	// Get all posts.
-	posts, err := db.GetPosts()
+	posts, err := db.GetPosts(parsedUUID)
 	if err != nil {
 		// Return, if posts not found.
+		fmt.Println(err.Error())
 		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
 			"error":   true,
 			"message": "posts were not found",
@@ -72,5 +81,186 @@ func GetPost(c *fiber.Ctx) error {
 		"error":   false,
 		"message": nil,
 		"data":    post,
+	})
+}
+
+func CreatePost(c *fiber.Ctx) error {
+
+	post := &models.Post{}
+	claims := utils.GetToken(c)
+	// Check, if received JSON data is valid.
+	if err := c.BodyParser(post); err != nil {
+		// Return status 400 and error message.
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error":   true,
+			"message": err.Error(),
+		})
+	}
+	// Create database connection.
+	db, err := configs.OpenDBConnection()
+	if err != nil {
+		// Return status 500 and database connection error.
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error":   true,
+			"message": err.Error(),
+		})
+	}
+
+	// Create a new validator for a Post model.
+	validate := validator.New()
+
+	// Assign data
+	post.ID = uuid.New()
+	post.CreatedAt = time.Now()
+	post.UserId = claims["id"].(uuid.UUID)
+
+	// Validate post fields.
+	if err := validate.Struct(post); err != nil {
+		// Return, if some fields are not valid.
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error":   true,
+			"message": utils.ValidatorErrors(err),
+		})
+	}
+	// Delete post by given ID.
+	if err := db.CreatePost(post); err != nil {
+		// Return status 500 and error message.
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error":   true,
+			"message": err.Error(),
+		})
+	}
+
+	// Return status 200 OK.
+	return c.JSON(fiber.Map{
+		"error":   false,
+		"message": nil,
+		"data":    post,
+	})
+}
+
+func UpdatePost(c *fiber.Ctx) error {
+	// Catch post ID from URL.
+	id, err := uuid.Parse(c.Params("id"))
+	claims := utils.GetToken(c)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error":   true,
+			"message": err.Error(),
+		})
+	}
+	// create new post struct
+	post := &models.PostUpdate{}
+
+	// Check, if received JSON data is valid.
+	if err := c.BodyParser(post); err != nil {
+		// Return status 400 and error message.
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error":   true,
+			"message": err.Error(),
+		})
+	}
+
+	// Create database connection.
+	db, err := configs.OpenDBConnection()
+	if err != nil {
+		// Return status 500 and database connection error.
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error":   true,
+			"message": err.Error(),
+		})
+	}
+
+	// Checking, if user with given ID does exist.
+	foundPost, err := db.GetPost(id)
+	if err != nil {
+		// Return status 404 and user not found error.
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": true,
+			"msg":   "user with this ID not found",
+		})
+	}
+	// Set initialized default data for user:
+	post.UpdatedAt = time.Now()
+
+	// Create a new validator for a user model.
+	validate := utils.NewValidator()
+
+	// Validate user fields.
+	if err := validate.Struct(post); err != nil {
+		// Return, if some fields are not valid.
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error":   true,
+			"message": utils.ValidatorErrors(err),
+		})
+	}
+	parsedUUID, _ := uuid.Parse(claims["id"].(string))
+	postAndUserId := &models.PostAndUserId{
+		ID:     foundPost.ID,
+		UserId: parsedUUID,
+	}
+	// Update user by given ID.
+	if err := db.UpdatePost(postAndUserId, post); err != nil {
+		// Return status 500 and error message.
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error":   true,
+			"message": err.Error(),
+		})
+	}
+
+	// Return status 200.
+	return c.JSON(fiber.Map{
+		"error":   false,
+		"message": "success",
+		"data":    nil,
+	})
+
+}
+
+func DeletePost(c *fiber.Ctx) error {
+	// Catch user ID from URL.
+	id, err := uuid.Parse(c.Params("id"))
+	claims := utils.GetToken(c)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error":   true,
+			"message": err.Error(),
+		})
+	}
+
+	// Create database connection.
+	db, err := configs.OpenDBConnection()
+	if err != nil {
+		// Return status 500 and database connection error.
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error":   true,
+			"message": err.Error(),
+		})
+	}
+
+	// Checking, if post with given ID does exist.
+	foundPost, err := db.GetPost(id)
+	if err != nil {
+		// Return status 404 and post not found error.
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error":   true,
+			"message": "post with this ID not found",
+		})
+	}
+
+	parsedUUID, _ := uuid.Parse(claims["id"].(string))
+	// Delete post by given ID.
+	if err := db.DeletePost(foundPost.ID, parsedUUID); err != nil {
+		// Return status 500 and error message.
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error":   true,
+			"message": err.Error(),
+		})
+	}
+
+	// Return status 204 no content.
+	return c.JSON(fiber.Map{
+		"error":   false,
+		"message": "success",
 	})
 }
--- pkg/models/post_model.go
+++ pkg/models/post_model.go
@@ -8,10 +8,22 @@ import (
 
 type Post struct {
 	ID        uuid.UUID `db:"id" json:"id" validate:"required,uuid"`
-	UserId    string    `db:"user_id" json:"user_id" validate:"required,uuid"`
+	UserId    uuid.UUID `db:"user_id" json:"user_id" validate:"required,uuid"`
 	Title     string    `db:"title" json:"title" validate:"required"`
 	Details   string    `db:"details" json:"details" validate:"required"`
 	IsPublish bool      `db:"is_publish" json:"is_publish"`
 	CreatedAt time.Time `db:"created_at" json:"created_at"`
 	UpdatedAt time.Time `db:"updated_at" json:"updated_at"`
+}
+
+type PostUpdate struct {
+	Title     string    `db:"title" json:"title"`
+	Details   string    `db:"details" json:"details"`
+	IsPublish bool      `db:"is_publish" json:"is_publish"`
+	UpdatedAt time.Time `db:"updated_at" json:"updated_at"`
+}
+
+type PostAndUserId struct {
+	ID     uuid.UUID `db:"id" json"id" validate:"required"`
+	UserId uuid.UUID `db:"user_id" json:"user_id"`
 }
